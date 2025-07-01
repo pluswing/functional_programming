58c34ebfa37d99f9da30d4d96e458596f8cbc4a6
pluswing <toshiyuki@pluswing.co.jp>
2025-07-01 10:10:18 +0900

    #002 neverthrowの使い方をざっくりと。

--- src/test.ts
+++ src/test.ts
@@ -1,19 +1,28 @@
-import { Err, ok, Ok, Result } from "neverthrow"
+import { err, Err, ok, Ok, Result } from "neverthrow"
 
-const a: string = "AAA"
-console.log(a)
-
-const divide = (a: number, b: number): number => {
+const divide = Result.fromThrowable((a: number, b: number): number => {
   if (b == 0) {
     throw new Error("zero divide error")
   }
   return a / b
-}
-// Result<number, Error>
+})
 
-try {
-  console.log(divide(1, 0))
-} catch (e) {
-  console.log(`ERR ${e}`)
+const multiply = (a: number, b: number): Result<number, Error> => {
+  // エラーが返る場合もあるよね。
+  return ok(a * b)
 }
 
+const v = multiply(2, 3).andThen((val) => divide(val, 2)).unwrapOr(10)
+console.log(v)
+
+// if (ret.isOk()) {
+//   console.log(ret.value)
+// }
+
+// const ret = multiply(2, 3)
+// if (ret.isOk()) {
+//   const n = divide(ret.value, 2)
+//   console.log(n)
+// } else {
+//   console.log(ret.error)
+// }
